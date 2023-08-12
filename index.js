a9c1c778a349e0f2cb2feb9a71dd50ddea095d81
Gabe <gabriel.velasquez0116@gmail.com>
2023-08-11 19:56:32 -0700

    started work on the license section and finished 3 functions

--- index.js
+++ index.js
@@ -63,7 +63,9 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,36 @@
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// If there is no license, return an empty string. Ternary operator evaluates if there is no license, it will be an empty string. if there is one, it will render license
+
+function renderLicenseBadge(license) {
+  (!license)? ``:`[![${license} license](https://img.shilds.io/badge/License-${license}-blue.svg)](${renderLicenseLink}(license)})`
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// If there is no license, return an empty string. Put it in a switch case to evaluate license
+function renderLicenseBadge(license) {
+  switch(license){
+    case 'MIT':
+      return `https://lbesson.mit-license.org/`;
+      break;
+    case 'GPL':
+      return `http://perso.crans.org/besson/LICENSE.html`;
+      break;
+    case 'CC--0':
+      return `https://creativecommons.org/licenses/by-nd/4.0`;
+      break;
+    default:
+      return ''; //returning empty string here if there is no valid license passed through function
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  (!license)?``:`##Licenses 
+  This project is using the ${license} license. To learn more, please click the license link listed above.`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
