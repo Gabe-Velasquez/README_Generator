28fc77e4651c5f0bcc6d8617d96e2e3414370642
Gabe <gabriel.velasquez0116@gmail.com>
2023-08-13 15:41:56 -0700

    formatted with prettier

--- index.js
+++ index.js
@@ -1,109 +1,127 @@
-
-const fs = require('fs'); //calling file system into my project so we can write a new file
-const inquirer = require('inquirer'); //installed inquirer package via npm and calling it into my project here
-const generateMarkdown = require('./utils/generateMarkdown'); //pulling required document for the template
+const fs = require("fs"); //calling file system into my project so we can write a new file
+const inquirer = require("inquirer"); //installed inquirer package via npm and calling it into my project here
+const generateMarkdown = require("./utils/generateMarkdown"); //pulling required document for the template
 
 // Using inquirer here to gather information from the user so we can use it later :)
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Welcome to this professional README generator!! Please enter your name?',
-            validate: inputName=>{ //added validate to all questions so there are values for each question
-                return (inputName) ? true 
-                : console.log('Please enter your name.');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'username',
-            message: 'What is your login on github?',
-            validate: inputGithub=>{
-                return (inputGithub) ? true 
-                : console.log('Please enter your github username.');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email?',
-            validate: inputEmail=>{
-                return (inputEmail) ? true 
-                : console.log('Please enter your email for your README.');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'projectTitle',
-            message: 'What is the title of your project?',
-            validate: inputTitle=>{
-                return (inputTitle) ? true 
-                : console.log('Your project must have a title.');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please write a description of your project here.',
-            validate: inputDescription=>{
-                return (inputDescription) ? true 
-                : console.log('Please enter a description of your project.');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Is there any special installation for this project?',
-            validate: inputInstallation=>{
-                return (inputInstallation) ? true 
-                : console.log('Please enter your installation steps to run this project.');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'How is this project supposed to be used?',
-            validate: inputUsage=>{
-                return (inputUsage) ? true 
-                : console.log('Please enter how your application is supposed to be used.');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Was there anyone who helped build this project? If so, who are they and what did they do?',
-            validate: inputContribution=>{
-                return (inputContribution) ? true 
-                : console.log('Please enter anyone that helped you build this project. If no one helped, put you as the "Sole Author"');
-                false
-            }
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'What tests were conducted in this project?',
-            validate: inputTests=>{
-                return (inputTests) ? true 
-                : console.log('Please list tests you used to build this project.');
-                false
-            }
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What license would you like to add to your project?',
-            choices: ['MIT','CC--0','GLP', 'No License'],
-        }
-    ]).then((answer)=>{
-        const markdownContent = generateMarkdown(answer);
-        fs.writeFile(`${answer.projectTitle}.md`, markdownContent,(err)=>{ //fs initiated and creates file while passing through markdown. added true false value for error, if its false, we indicate to user that we had successful build.
-            err?console.log(err):console.log('Successfully Built README')
-        });
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message:
+        "Welcome to this professional README generator!! Please enter your name?",
+      validate: (inputName) => {
+        //added validate to all questions so there are values for each question
+        return inputName ? true : console.log("Please enter your name.");
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your login on github?",
+      validate: (inputGithub) => {
+        return inputGithub
+          ? true
+          : console.log("Please enter your github username.");
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+      validate: (inputEmail) => {
+        return inputEmail
+          ? true
+          : console.log("Please enter your email for your README.");
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "projectTitle",
+      message: "What is the title of your project?",
+      validate: (inputTitle) => {
+        return inputTitle
+          ? true
+          : console.log("Your project must have a title.");
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please write a description of your project here.",
+      validate: (inputDescription) => {
+        return inputDescription
+          ? true
+          : console.log("Please enter a description of your project.");
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Is there any special installation for this project?",
+      validate: (inputInstallation) => {
+        return inputInstallation
+          ? true
+          : console.log(
+              "Please enter your installation steps to run this project."
+            );
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How is this project supposed to be used?",
+      validate: (inputUsage) => {
+        return inputUsage
+          ? true
+          : console.log(
+              "Please enter how your application is supposed to be used."
+            );
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message:
+        "Was there anyone who helped build this project? If so, who are they and what did they do?",
+      validate: (inputContribution) => {
+        return inputContribution
+          ? true
+          : console.log(
+              'Please enter anyone that helped you build this project. If no one helped, put you as the "Sole Author"'
+            );
+        false;
+      },
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What tests were conducted in this project?",
+      validate: (inputTests) => {
+        return inputTests
+          ? true
+          : console.log("Please list tests you used to build this project.");
+        false;
+      },
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "What license would you like to add to your project?",
+      choices: ["MIT", "CC--0", "GLP", "No License"],
+    },
+  ])
+  .then((answer) => {
+    const markdownContent = generateMarkdown(answer);
+    fs.writeFile(`${answer.projectTitle}.md`, markdownContent, (err) => {
+      //fs initiated and creates file while passing through markdown. added true false value for error, if its false, we indicate to user that we had successful build.
+      err ? console.log(err) : console.log("Successfully Built README");
     });
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,33 +1,39 @@
-const fs = require('fs');
+const fs = require("fs");
 // If there is no license, return an empty string. Ternary operator evaluates if there is no license, it will be an empty string. if there is one, it will render license badge
 function renderLicenseBadge(license) {
-  return (license ==='No License')?'':`[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  return license === "No License"
+    ? ""
+    : `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(
+        license
+      )})`;
 }
 
-// If there is no license, return an empty string. Put it in a switch case to evaluate license. Defaults to empty string if there are no matches for cases. 
+// If there is no license, return an empty string. Put it in a switch case to evaluate license. Defaults to empty string if there are no matches for cases.
 function renderLicenseLink(license) {
-  switch(license){
-    case 'MIT':
+  switch (license) {
+    case "MIT":
       return `https://mit-license.org/`;
       break;
-    case 'GPL':
+    case "GPL":
       return `https://www.gnu.org/licenses/gpl-3.0.en.html#license-text`;
       break;
-    case 'CC--0':
+    case "CC--0":
       return `https://creativecommons.org/licenses/by-nd/4.0`;
       break;
     default:
-      return ''; //returning empty string here if there is no valid license passed through function
+      return ""; //returning empty string here if there is no valid license passed through function
   }
 }
 
 // If license if strictly equal to 'No License' that we asked in inquirer, returns an empty string
 function renderLicenseSection(license) {
-  return (license ==='No License')?``:`
-  This project is using the ${license} license. To learn more, please click the license link listed above.`
+  return license === "No License"
+    ? ``
+    : `
+  This project is using the ${license} license. To learn more, please click the license link listed above.`;
 }
 
-//markdown is generated and we pass in all of the data that we collected from inquirer to create the readme file 
+//markdown is generated and we pass in all of the data that we collected from inquirer to create the readme file
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
   ${renderLicenseBadge(data.license)}
